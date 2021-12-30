7d15183e4cfd6b6d9f8fa450c4b1f00682167697
Thierry Maignan <maignan.thierry@outlook.fr>
2021-12-30 14:03:54 +0100

    fixed rogue like and rpgsmart

--- RPGSmart.py
+++ RPGSmart.py
@@ -4,39 +4,46 @@ from fight import encounter
 from rogue_like import Monster_Encounter
 from rogue_like import Player_Movement
 from rogue_like import world_map
-from rogue_like import Monster_Movement
+from rogue_like import Goblin_Movement
 
 
 def Main():
-    Monster_Chance = Monster_Encounter()
+    from Menu import main_menu
+    from shop import fonction_shop
+    from fight import encounter
+    from rogue_like import Monster_Encounter, Player_Movement, world_map, Monster_Movement
+
     inventaire = []
     argent = 50
-
-    liste_item = ["Potion", "item2", "item3"]
+    liste_item = ["Potion", "Bombe", "item3"]
     liste_prix = [5, 10, 15]
     y = 19
     x = 14
+    gobliny = 19
+    goblinx = 7
     monstery = 9
     monsterx = 10
     Player_Life = 3
 
     print("Pour bouger le personnage:")
     print(" -Haut  -bas \n -gauche  -droite")
-    world_map(y, x)
+    world_map(y, x, monstery, monsterx, gobliny, goblinx)
     while Player_Life > 0:
         Move = input("Entrez une direction: ")
         y, x = Player_Movement(Move, y, x)
         monstery, monsterx = Monster_Movement(monstery, monsterx)
+        gobliny, goblinx = Goblin_Movement(gobliny, goblinx)
         if y == monstery and x == monsterx:
             Monster_Encounter()
         if y == 16 and x == 13:
             inventaire, argent = fonction_shop(
                 inventaire, argent, liste_item, liste_prix)
+
         else:
             Monster_Chance = Monster_Encounter()
             if Monster_Chance < 15:
                 encounter()
-        world_map(y, x)
+        world_map(y, x, monstery, monsterx, gobliny, goblinx)
 
 
 Main()
--- rogue_like.py
+++ rogue_like.py
@@ -1,6 +1,5 @@
-from random import randint
 from os import system
-
+from random import randint
 Player = "😎"
 Final_Boss = "💀"
 Boss1 = "🤩"
@@ -10,87 +9,120 @@ Shop1 = "💰"
 Shop2 = "💰"
 Master_Roshi = "🥸 "
 Monster = "😡"
-y = 19
-x = 14
-monstery = 9
-monsterx = 10
+Goblin = "🤑"
 
-#---------------------------------------------------------------------
-def Monster_Movement(monstery,monsterx):
-    Index1 = randint(1,3)
-    Index2 = randint(1,3)
+
+# ---------------------------------------------------------------------
+
+def Goblin_Movement(gobliny, goblinx):
+    Index1 = randint(1, 3)
+    Index2 = randint(1, 3)
+    if Index1 == 1:
+        if gobliny < 19:
+            gobliny += 1
+        else:
+            pass
+    if Index1 == 2:
+        if gobliny > 16:
+            gobliny -= 1
+        else:
+            pass
+    if Index2 == 1:
+        if goblinx < 12:
+            goblinx += 1
+        else:
+            pass
+    if Index2 == 2:
+        if goblinx > 0:
+            goblinx -= 1
+        else:
+            pass
+    return gobliny, goblinx
+
+# ---------------------------------------------------------------------
+
+
+def Monster_Movement(monstery, monsterx):
+    Index1 = randint(1, 3)
+    Index2 = randint(1, 3)
     if Index1 == 1:
         if monstery < 12:
-            monstery+=1
+            monstery += 1
         else:
             pass
     if Index1 == 2:
         if monstery > 7:
-            monstery-=1
-        else: 
-            pass 
-    if Index2 == 1: 
-        if monsterx < 13:
-            monsterx+=1
+            monstery -= 1
         else:
-            pass 
+            pass
+    if Index2 == 1:
+        if monsterx < 13:
+            monsterx += 1
+        else:
+            pass
     if Index2 == 2:
         if monsterx > 0:
-            monsterx-=1
+            monsterx -= 1
         else:
-            pass 
-    return monstery,monsterx
+            pass
+    return monstery, monsterx
 
-#---------------------------------------------------------------------
+# ---------------------------------------------------------------------
+
+
 def Monster_Encounter():
-    Monster_Chance = randint(0,50)
+    Monster_Chance = randint(0, 50)
     return Monster_Chance
 
-#---------------------------------------------------------------------
-def Player_Movement(Move,y,x):
+# ---------------------------------------------------------------------
+
+
+def Player_Movement(Move, y, x):
     Move = Move.strip()
     system("clear")
     if Move == "haut":
-        if y != 0:       
-            y -= 1  
+        if y != 0:
+            y -= 1
         else:
             print("Vous ne pouvez pas aller en haut")
-            pass        
+            pass
 
     if Move == "bas":
-        if y < 19:            
-            y += 1    
+        if y < 19:
+            y += 1
         else:
-            print("Vous ne pouvez pas aller en bas")        
+            print("Vous ne pouvez pas aller en bas")
             pass
 
     if Move == "gauche":
-        if x != 0:            
-            x -= 1     
+        if x != 0:
+            x -= 1
         else:
             print("Vous ne pouvez pas aller a gauche")
             pass
 
     if Move == "droite":
-        if x <14:            
-            x += 1        
+        if x < 14:
+            x += 1
         else:
             print("Vous ne pouvez pas aller a droite")
-            pass    
-    return y,x 
+            pass
+    return y, x
 
-#---------------------------------------------------------------------
-def world_map(y,x):
+# ---------------------------------------------------------------------
+
+
+def world_map(y, x, monstery, monsterx, gobliny, goblinx):
     i = 0
     p = 0
     World_Map = []
     Map = []
-    for i in range (18):
+    for i in range(18):
         L = []
         L1 = []
-        for p in range (15):
+        for p in range(15):
             L.append("|__|")
-            p += 1 
+            p += 1
         i += 1
         Map.append(L)
         if i == 13 or i == 6:
@@ -101,16 +133,13 @@ def world_map(y,x):
     Map[3][10] = f"|{Shop2}|"
     Map[15][0] = f"|{Boss1}|"
     Map[7][14] = f"|{Boss2}|"
-    Map[0][0] = f"|{Final_Boss}|" 
-    Map[2][6] = f"|{Master_Roshi}|" 
+    Map[0][0] = f"|{Final_Boss}|"
+    Map[2][6] = f"|{Master_Roshi}|"
     Map[monstery][monsterx] = f"|{Monster}|"
+    Map[gobliny][goblinx] = f"|{Goblin}|"
     a = 0
-    for a in range (len(Map)):
+    for a in range(len(Map)):
         World_Map.append(Map[a])
         print(*World_Map[a])
         a += 1
     return Map
-
-
-monstery,monsterx = Monster_Movement(monstery,monsterx)
-Map = world_map(y,x)
